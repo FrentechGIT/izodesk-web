2c97964948d39e844aae4006ece7f68031b0946e
FrentechGIT <kontakt@frentech.org>
2026-03-30 18:20:21 +0200

    test1

---
+++ dist/_next/static/chunks/app/layout-cedfeb3c8dadd294.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{779:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_1b85de"}},1290:()=>{},5771:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,779,23)),Promise.resolve().then(l.t.bind(l,1290,23))}},e=>{e.O(0,[131,441,255,358],()=>e(e.s=5771)),_N_E=e.O()}]);
