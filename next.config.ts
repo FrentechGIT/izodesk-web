93cf2e520a10f533db0cc699c55e58161066e861
Tomáš <kontakt@frentech.org>
2026-03-30 18:22:11 +0200

    Revert "test1"
    This reverts commit 2c97964948d39e844aae4006ece7f68031b0946e.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,3 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
-  output: "export",
-  distDir: "dist"
-};
+const nextConfig: NextConfig = {};
 export default nextConfig;
-
